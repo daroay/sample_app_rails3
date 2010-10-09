8b359fca34a0f2f2eda2a8f71ec95e9429b9e2d7
aiionx <aion.x@hotmail.com>
2010-10-09 12:23:47 -0500

    Finished layout and routes

--- spec/requests/layout_lynks_spec.rb
+++ spec/requests/layout_lynks_spec.rb
@@ -22,5 +22,10 @@ describe "LayoutLynks" do
       get '/help'
       response.should have_selector('title', :content => "Help")
     end
+
+    it "should have a Signup page at '/signup'" do
+      get '/signup'
+      response.should have_selector('title', :content => "Sign up")
+    end
  
 end
